694d580bca7f4c0ed91dcdfccfa3dd3a091ffdde
Chandresh Prajapati <pchandreshr7@gmail.com>
2025-08-15 21:09:29 +0000

    use api for show latest movies

--- imdb-clone/src/components/Home.jsx
+++ imdb-clone/src/components/Home.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Banner from '../home/Banner'
 import MovieList from '../home/MovieList'
+import axios from 'axios'
 
 function Home() {
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=de129c78859f307157260a20a0534c74&language=en-US&page=1`).then(function(res){
+      console.log(res.data.results)
+      setMovies(res.data.results)
+    })
+  }, [])
   return (
     <>
       <Banner />
       <div className='text-center text-3xl p-5'>Trending Movie's</div>
       <div className='flex flex-row flex-wrap justify-around'>
-        <MovieList />
-        <MovieList />
-        <MovieList />
-        <MovieList />
-        <MovieList />
-        <MovieList />
+        {
+          movies.map((movieObj) => {
+            return <MovieList poster_path={movieObj.poster_path} original_title={movieObj.original_title} />
+
+          })
+        }
       </div>
     </>
   )
--- imdb-clone/src/home/MovieList.jsx
+++ imdb-clone/src/home/MovieList.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function MovieList() {
+function MovieList({poster_path, original_title}) {
     return (
         <div className='p-5'>
-            <div className='h-[40vh] w-48 flex bg-cover bg-center rounded hover:scale-110 duration-300 hover:cursor-pointer' style={{ backgroundImage: `URL('https://images.atomtickets.com/image/upload/w_520,h_780,q_auto/v1/ingestion-images-archive-prod/archive/a64833819493.jpg')` }}>
-
+            <div className='h-[40vh] w-48 flex bg-cover bg-center rounded hover:scale-110 duration-300 hover:cursor-pointer' style={{ backgroundImage: `URL(https://image.tmdb.org/t/p/original/${poster_path})` }}>
+                <div className='text-white p-2 bg-gray-900/60 text-center w-full'>{original_title}</div>
             </div>
         </div>
     )
