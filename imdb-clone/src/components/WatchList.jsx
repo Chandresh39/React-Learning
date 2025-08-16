3c17c77771f68eb47ce32b424dfe8a3cea488e43
Chandresh Prajapati <pchandreshr7@gmail.com>
2025-08-16 13:37:51 +0000

    design whatchlist web page

--- imdb-clone/src/components/WatchList.jsx
+++ imdb-clone/src/components/WatchList.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 
 function WatchList() {
   return (
-    <div>
-      <h1>WatchList component</h1>
+    <>
+    <div className='p-3 justify-center flex'>
+      <input type="text" placeholder='Search Movies' className='bg-gray-100 p-2 w-60 h-10 border rounded' />
     </div>
+    
+    <div className='flex flex-row flex-wrap justify-around'>
+      <button className='border text-white bg-blue-500 p-1 m-1 rounded hover:opacity-70'>All Genre</button>
+    </div>
+
+    <div>
+      <table className='w-full p-2 border rounded'>
+        <thead>
+          <tr>
+            <th>Sr. No.</th>
+            <th>Poster</th>
+            <th>Title</th>
+            <th>Rating</th>
+            <th>Genre</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody className=''>
+          <tr>
+            <td>1</td>
+            <td className='bg-cover bg-center h-40 w-auto' style={{
+                    backgroundImage: `url('https://english.cdn.zeenews.com/sites/default/files/2025/01/16/1635505-jh234-2025-01-16t130115.212.jpg')`,
+                }}></td>
+            <td>Narshimha</td>
+            <td>9.7</td>
+            <td>Action, Animation</td>
+            <td><button className='text-white bg-red-500 hover:opacity-70 rounded p-1 hover:cursor-pointer'>Delete</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    </>
   )
 }
 
--- imdb-clone/src/home/MovieList.jsx
+++ imdb-clone/src/home/MovieList.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
-function MovieList({poster_path, original_title}) {
-    return (
-        <div className='p-5'>
-            <div className='h-[40vh] w-48 flex bg-cover bg-center rounded hover:scale-110 duration-300 hover:cursor-pointer' style={{ backgroundImage: `URL(https://image.tmdb.org/t/p/original/${poster_path})` }}>
-                <div className='text-white p-2 bg-gray-900/60 text-center w-full'>{original_title}</div>
-            </div>
+function MovieList({ poster_path, original_title }) {
+  return (
+    <div className="p-5">
+      <div
+        className="h-[40vh] w-48 flex bg-cover bg-center rounded relative hover:scale-110 duration-300 hover:cursor-pointer group"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${poster_path})`,
+        }}
+      >
+        <div className="absolute bottom-0 left-0 right-0 text-white p-2 bg-gray-900/70 text-center opacity-0 group-hover:opacity-100 duration-300">
+          {original_title}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default MovieList
